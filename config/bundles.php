c1a86d44c6d658f52d429008902470a87d0bd51c
mashero <dima.vuynov@yandex.ru>
2024-05-03 15:59:43 +0300

    Добавлен vich uploader для связи easyadmin с flysystem

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     League\FlysystemBundle\FlysystemBundle::class => ['all' => true],
+    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
 ];
